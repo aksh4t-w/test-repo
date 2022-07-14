a587509636254d6eabcd927b8a76d0b588a4322a
akshatw <akshatwagadre007@gmail.com>
2022-07-14 11:01:03 -0500

    PWA test 5

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 // Install SW
-self.addEventListener('install', event => {
-    console.log("Service worker installed")
-})
+self.addEventListener("install", (event) => {
+  console.log("Service worker installed");
+});
 
-// Activate SW 
-self.addEventListener('activate', event => {
-    console.log("Service worker activated")
-})
+// Activate SW
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activated");
+});
 
-self.addEventListener( "fetch", event => {
-    console.log('WORKER: Fetching requests');
-  });
+self.addEventListener("fetch", (event) => {
+  console.log("WORKER: Fetching requests");
+});
